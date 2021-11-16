c725ef0e72e515c1ebb97872524bc34dad3616d6
Sandydht <sandydwi75@gmail.com>
2021-11-16 20:35:51 +0700

    Style the AppBar component

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Club Finder</h1>
+  <div class="py-3 px-5 bg-indigo-600 shadow-md">
+    <h1 class="font-bold text-xl text-white">Club Finder</h1>
   </div>
 </template>
 
